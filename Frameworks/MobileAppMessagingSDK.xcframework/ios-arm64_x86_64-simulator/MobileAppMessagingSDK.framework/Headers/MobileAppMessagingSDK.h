0b943c2a0d446dd3dbeeb3bc28ea5068299627b1
Himadri Sekhar Jyoti <hjyoti@salesforce.com>
2025-11-06 18:48:29 +0530

    v1.0.0

---
+++ Frameworks/MobileAppMessagingSDK.xcframework/ios-arm64_x86_64-simulator/MobileAppMessagingSDK.framework/Headers/MobileAppMessagingSDK.h
@@ -0,0 +1,20 @@
+//
+//  MobileAppMessagingSDK.h
+//  MobileAppMessagingSDK
+//
+//  Copyright © 2025 Salesforce Marketing Cloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobileAppMessagingSDK.
+FOUNDATION_EXPORT double MobileAppMessagingSDKVersionNumber;
+
+//! Project version string for MobileAppMessagingSDK.
+FOUNDATION_EXPORT const unsigned char MobileAppMessagingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileAppMessagingSDK/PublicHeader.h>
+
+#if __has_include(<MobileAppMessagingSDK/MobileAppMessagingSDK-Swift.h>)
+#import <MobileAppMessagingSDK/MobileAppMessagingSDK-Swift.h>
+#endif
---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SFMobileAppMessagingSDK",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "MobileAppMessagingSDK",
+            targets: ["MobileAppMessagingSDKWrapper"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/salesforce-marketingcloud/sfmc-sdk-ios", from: "3.0.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MobileAppMessagingSDK",
+            path: "Frameworks/MobileAppMessagingSDK.xcframework"
+        ),
+        .target(
+            name: "MobileAppMessagingSDKWrapper",
+            dependencies: [
+                .target(name: "MobileAppMessagingSDK"),
+                .product(name: "SFMCSDK", package: "sfmc-sdk-ios")
+            ],
+            path: "MobileAppMessagingSDK"
+        )
+    ]
+)
